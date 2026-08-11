6e45b44e9fd8a4a9737b7fe91c9c47a54855a7e8
Per Johansson <pelleindc@gmail.com>
2026-08-10 21:34:30 -0600

    Organize Spain itinerary references

--- spain-2026/service-worker.js
+++ spain-2026/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'spain-trip-v2';
+const CACHE_NAME = 'spain-trip-v3';
 const APP_SHELL = ['./', './index.html', './styles.css', './app.js', './auth.js', './manifest.webmanifest', './icon-192.png', './icon-512.png', './photos/united-confirmation.jpg', './photos/scheduled-1.jpg', './photos/scheduled-2.jpg', './photos/olivia-chloe-return.png', './photos/royal-palm-22.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key.startsWith('spain-trip-') && key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim())));
